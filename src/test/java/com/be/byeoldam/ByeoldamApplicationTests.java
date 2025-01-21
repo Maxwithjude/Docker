6d47755bba90f87d97a419845661a5e677009bfa
eenzzi <purelej99@gmail.com>
2025-01-21 12:54:10 +0900

    프로젝트 생성

---
+++ src/test/java/com/be/byeoldam/ByeoldamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.byeoldam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByeoldamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

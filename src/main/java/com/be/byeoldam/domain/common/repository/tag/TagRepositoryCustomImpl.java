acf5c70019055d191d24f3ca63808583aaa40d72
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-01-24 15:22:21 +0900

    feat: Tag- QueryDSL Test추가

--- src/main/java/com/be/byeoldam/domain/common/repository/tag/TagRepositoryCustomImpl.java
+++ src/main/java/com/be/byeoldam/domain/common/repository/tag/TagRepositoryCustomImpl.java
@@ -3,23 +3,24 @@ package com.be.byeoldam.domain.common.repository.tag;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import static com.be.byeoldam.domain.common.model.QTag.tag;
 
 
 @Repository
 public class TagRepositoryCustomImpl implements TagRepositoryCustom{
 
-
     @Autowired
     private JPAQueryFactory queryFactory;
 
-  //  private final QTag tag =
-
+    @Transactional
     @Override
     public void decrementReferenceCountByName(String name) {
-//        queryFactory.update(tag)  // 업데이트 대상은 Tag 엔티티
-//                .set(tag.referenceCount, tag.referenceCount.subtract(1))  // referenceCount 1 감소
-//                .where(tag.name.eq(name))  // 조건: name을 기준으로
-//                .execute();  // 쿼리 실행
+        queryFactory.update(tag)  // 업데이트 대상은 Tag 엔티티
+                .set(tag.referenceCount, tag.referenceCount.subtract(1))  // referenceCount 1 감소
+                .where(tag.name.eq(name))  // 조건: name을 기준으로
+                .execute();  // 쿼리 실행
     }
+
 }
--- src/test/java/com/be/byeoldam/tag/TagTest.java
+++ src/test/java/com/be/byeoldam/tag/TagTest.java
@@ -1,26 +1,32 @@
 package com.be.byeoldam.tag;
 
 import com.be.byeoldam.config.AuditingConfig;
+import com.be.byeoldam.config.QuerydslConfig;
 import com.be.byeoldam.domain.common.model.Tag;
 import com.be.byeoldam.domain.common.repository.tag.TagRepository;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+//@SpringBootTest
 @DataJpaTest
-@Import(AuditingConfig.class)
+@Import({AuditingConfig.class, QuerydslConfig.class})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class TagTest {
     @Autowired
     TagRepository tagRepository;
+    @Autowired
+    EntityManager entityManager; // EntityManager 주입
 
     @Test
-    @Rollback(false)
+    @Rollback
     void testSaveAndFindTag() {
         // Given
         String tagName = "공부";
@@ -34,10 +40,33 @@ public class TagTest {
 
         // Then: 저장된 Tag와 조회된 Tag가 동일한지 검증
         assertThat(foundTag.getName()).isEqualTo(tagName);  // 이름이 동일해야 한다
-        assertThat(foundTag.getReferenceCount()).isEqualTo(0);  // 기본값이 0이어야 한다
+        assertThat(foundTag.getReferenceCount()).isEqualTo(1);  // 기본값이 0이어야 한다
         assertThat(foundTag.getColor()).isEqualTo(savedTag.getColor()); // 색상이 null이 아니어야 한다 (랜덤 값)
         String color = foundTag.getColor().toString();
-        System.out.println("색상:" + color);
+    }
+
+
+    @Test
+    @Transactional
+    @Rollback
+    void testDecrementReferenceCountByName() {
+        // Given: 새로운 Tag 객체를 생성하고 DB에 저장
+        String tagName = "공부3";
+        Tag tag = Tag.builder()
+                .name(tagName)
+                .build();
+        tagRepository.save(tag);
+
+        // EntityManager와 QueryDSL이 동기화 되지 않음, 동기화 해주기.
+        entityManager.flush();
+        entityManager.clear();
+
+        // When: Tag의 referenceCount를 1 감소시킴
+        tagRepository.decrementReferenceCountByName(tagName);
+
+        // Then: referenceCount가 1 감소했는지 검증
+        Tag updatedTag = tagRepository.findById(tag.getId()).orElse(null);
+        assertThat(updatedTag.getReferenceCount()).isEqualTo(0);
     }
 
 }

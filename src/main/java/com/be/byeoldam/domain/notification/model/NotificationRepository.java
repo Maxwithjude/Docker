0fa568133d3ca0303008510bf3401e45749989d3
eenzzi <purelej99@gmail.com>
2025-01-24 14:39:33 +0900

    알림 리포지토리 생성

---
+++ src/main/java/com/be/byeoldam/domain/notification/model/NotificationRepository.java
@@ -0,0 +1,6 @@
+package com.be.byeoldam.domain.notification.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}

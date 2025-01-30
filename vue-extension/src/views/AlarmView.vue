93b0c4e973566b309198c55ae745aede1c408b5f
LeeSeongHui <seonghui3472@gmail.com>
2025-01-31 07:41:23 +0900

    manifest 파일 설정 완료

---
+++ vue-extension/src/views/AlarmView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>알림리스트 조회 페이지</div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ vue-extension/src/views/FeedView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>피드리스트 조회 페이지</div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>

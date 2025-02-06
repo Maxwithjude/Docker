b01d4265f2bc4e4388e7088de9053db9296dc0ef
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-02-06 10:03:18 +0900

    feat: JWT

---
+++ src/test/java/com/be/byeoldam/user/JwtUtilTest.java
@@ -0,0 +1,36 @@
+package com.be.byeoldam.user;
+
+import com.be.byeoldam.domain.user.jwt.JWTUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class JwtUtilTest {
+    private JWTUtil jwtUtil;
+    private String token;
+    private final long userId = 1L;
+    private final String email = "test@example.com";
+    private final String role = "USER";
+    private final long expirationMs = 60000L; // 1분
+
+    @BeforeEach
+    void setUp() {
+        jwtUtil = new JWTUtil();
+        token = jwtUtil.createJwt(userId, email, role, expirationMs);
+    }
+
+    @Test
+    @DisplayName("JWT 기능 테스트")
+    void testJwt(){
+        Long extractedUserId = jwtUtil.getUserId(token);
+        String extractedEmail = jwtUtil.getUserEmail(token);
+        String extractedRole = jwtUtil.getRole(token);
+
+        assertEquals(userId, extractedUserId);
+        assertEquals(email, extractedEmail);
+        assertEquals(role, extractedRole);
+    }
+}

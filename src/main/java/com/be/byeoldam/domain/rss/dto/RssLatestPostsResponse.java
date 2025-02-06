1c1c9f10d0bb45d0a7c8bc18e9a03048a38224eb
eenzzi <purelej99@gmail.com>
2025-02-06 10:05:18 +0900

    latestArticles -> latestPosts 변경

--- src/main/java/com/be/byeoldam/domain/rss/dto/RssLatestPostsResponse.java
+++ src/main/java/com/be/byeoldam/domain/rss/dto/RssLatestPostsResponse.java
@@ -4,21 +4,19 @@ import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
 
-import java.util.List;
-
 @Getter
 @Builder
 public class RssLatestPostsResponse {
 
     private Long rssId;
     private String name;
-    private Page<RssPostResponse> latestArticles;
+    private Page<RssPostResponse> latestPosts;
 
-    public static RssLatestPostsResponse of(Long rssId, String name, Page<RssPostResponse> latestArticles) {
+    public static RssLatestPostsResponse of(Long rssId, String name, Page<RssPostResponse> latestPosts) {
         return RssLatestPostsResponse.builder()
                 .rssId(rssId)
                 .name(name)
-                .latestArticles(latestArticles)
+                .latestPosts(latestPosts)
                 .build();
     }
 }

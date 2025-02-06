4c1062db52cba2c9ec4433e7ee2721251fbe1a4a
eenzzi <purelej99@gmail.com>
2025-02-06 14:37:12 +0900

    북마크 리스트 조회 메서드 추가

--- src/main/java/com/be/byeoldam/domain/memo/MemoRepository.java
+++ src/main/java/com/be/byeoldam/domain/memo/MemoRepository.java
@@ -3,5 +3,8 @@ package com.be.byeoldam.domain.memo;
 import com.be.byeoldam.domain.memo.model.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findByBookmarkId(Long bookmarkId);
 }

afdbb35628475614ec076404dd9ae8aa30109207
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-02-07 18:31:04 +0900

    feat: cors문제 해결

--- src/main/java/com/be/byeoldam/config/WebConfig.java
+++ src/main/java/com/be/byeoldam/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.be.byeoldam.config;
 import com.be.byeoldam.common.resolver.LoginUserResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,6 +18,15 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://127.0.0.1:8080", "http://localhost:5173", "http://localhost:5174") // Swagger UI와 관련된 Origin 허용
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "PUT") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true); // 인증 정보(쿠키 등) 허용
+    }
+
+        @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserResolver);
     }

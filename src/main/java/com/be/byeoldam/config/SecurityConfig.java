8454dde298436d9ba91015112c13106735fa5ed0
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-02-11 15:29:31 +0900

    refact: users미안!

--- src/main/java/com/be/byeoldam/config/SecurityConfig.java
+++ src/main/java/com/be/byeoldam/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
          * */
         http.authorizeHttpRequests((auth) -> auth
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/v3/api-docs.yaml", "/v3/api-docs/swagger-config").permitAll() //swagger
-                .requestMatchers("/login", "api/users/register","api/users/refresh").permitAll() //모든 사용자
+                .requestMatchers("/login", "api/users/register","api/users/refresh","api/users/email/send").permitAll() //모든 사용자
                 .requestMatchers("/admin").hasRole("ADMIN") //role이 ADMIN인 유저만(추후에 수정하기)
                 .requestMatchers("/","/api/test").permitAll()
                 .anyRequest().authenticated()); //나머지는 로그인한 유저만
@@ -72,7 +72,7 @@ public class SecurityConfig {
 //       http.addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository), LogoutFilter.class);
 
         LoginFilter loginFilter = new LoginFilter(authenticationManager(), jwtUtil, userService);
-        loginFilter.setFilterProcessesUrl("/api/user/login");
+        loginFilter.setFilterProcessesUrl("/api/users/login");
         http.addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class);
         http.addFilterBefore(new JWTFilter(jwtUtil, userDetailsService), LoginFilter.class);
 

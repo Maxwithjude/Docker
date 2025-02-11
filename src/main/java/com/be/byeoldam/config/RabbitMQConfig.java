6f3c1824467752ab69d54e5790c32aa59dfacdf2
eenzzi <purelej99@gmail.com>
2025-02-11 13:30:50 +0900

    큐 이름 변경 및 설정 추가

--- src/main/java/com/be/byeoldam/config/RabbitMQConfig.java
+++ src/main/java/com/be/byeoldam/config/RabbitMQConfig.java
@@ -1,34 +1,40 @@
 package com.be.byeoldam.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
+    private static final String QUEUE_NAME = "notification.queue";
+    private static final String EXCHANGE_NAME = "notification.exchange";
+    private static final String ROUTING_KEY = "notification.key";
+
+    @Bean
+    public Queue notificationQueue() {
+        return new Queue(QUEUE_NAME, true);  // durable=true -> 서버 재시작해도 큐 유지
+    }
+
     @Bean
     public DirectExchange notificationExchange() {
-        return new DirectExchange("notification.exchange");
+        return new DirectExchange(EXCHANGE_NAME);
     }
 
     @Bean
-    public Queue bookmarkRemindQueue() {
-        return new Queue("bookmark.remind.queue");
+    public Binding notificationBinding(Queue notificationQueue, DirectExchange notificationExchange) {
+        return BindingBuilder.bind(notificationQueue).to(notificationExchange).with(ROUTING_KEY);
     }
 
     @Bean
-    public Queue inviteNotificationQueue() {
-        return new Queue("invite.notification.queue");
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
     }
 
-    @Bean
-    public Binding bookmarkRemindBinding(DirectExchange exchange, Queue bookmarkRemindQueue) {
-        return BindingBuilder.bind(bookmarkRemindQueue).to(exchange).with("bookmark.remind");
-    }
 
-    @Bean
-    public Binding inviteNotificationBinding(DirectExchange exchange, Queue inviteNotificationQueue) {
-        return BindingBuilder.bind(inviteNotificationQueue).to(exchange).with("invite.notification");
-    }
 }
